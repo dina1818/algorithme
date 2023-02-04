0a4a81e19448f3e63ba6898a647c9f7dc116ba41
dina1818 <dina.chihaoui@esprit.tn>
2023-02-04 14:19:11 +0100

    first commit

---
+++ New Text Document.c
@@ -0,0 +1,30 @@
+algorithme checkpoint
+    msg : string;
+i : integer;
+n : integer
+        begin
+            write("ecrire une phrase");
+read(msg);
+lenght(msg);
+total = 0;
+for (i = 0; i < lenght(msg); i++)
+{
+
+    if msg
+        [i] = " " then total = total + 1;
+}
+SWITCH(msg)
+DO case "a":
+n + 1;
+case "e":
+n + 1;
+case "i":
+n + 1;
+case "o":
+n + 1;
+"y" : set_instructions case "u":
+n + 1;
+default: no voyoleles
+    END_SWITCH
+        write "le nombrede voyelles est n ";
+end
